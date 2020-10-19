5fe41f5458ee1cf7d57a42c131123adce1032382
jod35 <jod35@test.com>
2020-10-19 15:14:49 +0300

    changes to settings to change allowed hosts

--- coroblog/settings.py
+++ coroblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c(ii7_o20s6$e@j**s@oe#8r3&xg9afhzyh#zjc_mz+utle_3$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
