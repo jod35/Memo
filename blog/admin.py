3752c5e95fdf2a78aa71e2c4917fdd393dc7698b
jod35 <jod35@test.com>
2020-10-20 14:41:31 +0300

    registered the post model with the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title','author','created','publish',]
+    prepopulated_fields={'slug':('title',)}
+    list_filter=['created','publish',]
+    
