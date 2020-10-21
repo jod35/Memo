bc7c6ffd8110b2651ef78abd2eeeeca15cfc445a
jod35 <jod35@test.com>
2020-10-21 22:09:50 +0300

    create a view to display all uer posts

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from . models import Post
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -9,5 +10,9 @@ class UserRegistrationForm(UserCreationForm):
         model=User
         fields=['username','email','password1','password2']
 
+class PostCreationForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=['title','body']
 
 
