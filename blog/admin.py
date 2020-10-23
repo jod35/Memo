8e1ebb7c50eb64d2d6b7e0cfe7a1830b9ee67a97
jod35 <jod35@test.com>
2020-10-23 14:34:46 +0300

    register profile model with admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Profile
 
 
 @admin.register(Post)
@@ -7,4 +7,9 @@ class PostAdmin(admin.ModelAdmin):
     list_display=['title','author','created','publish',]
     prepopulated_fields={'slug':('title',)}
     list_filter=['created','publish',]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=['user']
     
